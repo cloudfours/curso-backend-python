739e059f4c0fdd85b4f4126e4be0cdf1d423f156
angel santibañez <38432709+cloudfours@users.noreply.github.com>
2023-03-19 16:14:17 -0500

    Update model.py

--- schema/model.py
+++ schema/model.py
@@ -4,7 +4,7 @@ class User(BaseModel):
     email:str
     password:str
 class Movie(BaseModel):
-    id: Optional[int] | None=Field(gt=1,le=10,default=1)
+    id: int | None=Field(gt=1,le=10,default=1)
     title:str | None=Field(max_length=30,min_length=5)
     overview:str | None=Field(max_length=50,min_length=5)
     year:int | None=Field(le=2023, default=2023)
