70e1e7e2237b7bc00bbdfc9596ca8309ae165c2f
angel santibañez <38432709+cloudfours@users.noreply.github.com>
2023-03-19 15:37:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi import Depends, FastAPI, Body, Path, Query, HTTPException
 from middlaware.error_handler import Errorhandler
 from apps.movies import appRouter
 from security.login import appRouterLogin
+import uvicorn
+import os
+
 app = FastAPI()
 app.title = "mi app con fastApi"
 app.version = "2.5"
@@ -10,3 +13,7 @@ app.add_middleware(Errorhandler)
 app.include_router(appRouterLogin)
 
 app.include_router(appRouter)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
